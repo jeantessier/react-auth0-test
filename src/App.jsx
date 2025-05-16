23f46b92111872c797386ead56590cda48f34180
Jean Tessier <jean@jeantessier.com>
2025-05-16 12:30:20 -0700

    Show/hide buttons based on authentication status

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import LoginButton from './LoginButton'
 import LogoutButton from './LogoutButton'
 import Profile from './Profile'
 import './App.css'
 
 function App() {
+    const { isAuthenticated } = useAuth0()
+
     return (
 	<>
 	    <h1>React Auth0 Test</h1>
-	    <div>
-		<LoginButton/>
-		<LogoutButton/>
-	    </div>
+	    {!isAuthenticated && <div><LoginButton/></div>}
 	    <Profile/>
+	    {isAuthenticated && <div><LogoutButton/></div>}
 	</>
     )
 }
