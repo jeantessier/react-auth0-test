a3e88d247784012f1f22f241445593ba5d59c9b3
Jean Tessier <jean@jeantessier.com>
2025-05-16 12:16:52 -0700

    Add LoginButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import LoginButton from './LoginButton'
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <h1>React Auth0 Test</h1>
-    </>
-  )
+    return (
+	<>
+	    <h1>React Auth0 Test</h1>
+	    <LoginButton/>
+	</>
+    )
 }
 
 export default App
